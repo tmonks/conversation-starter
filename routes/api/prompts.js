3a83fcdfddd77f7fe582fb906668590942329656
Tom Monks <monksy@gmail.com>
2020-02-07 15:03:56 -0500

    /api/prompts/random route

--- routes/api/prompts.js
+++ routes/api/prompts.js
@@ -4,18 +4,48 @@ const router = express.Router();
 // Prompt model
 const Prompt = require("../../models/Prompt");
 
+// ObjectId
+const ObjectId = require("mongoose").Types.ObjectId;
+
 // @route   GET api/prompts
 // @desc    GET all Prompts
 // @access  Public
 router.get("/", (req, res) => {
-  console.log("GET request received for prompts");
-  // res.json({ error: "GET route not fully baked" });
-  Prompt.find()
+  console.log("GET request received for prompts with category_id of " + req.query.category_id);
+  Prompt.find(req.query.category_id ? { category: req.query.category_id } : {})
     .populate("category", "title -_id")
     .then(results => res.send(results))
     .catch(err => {
       console.log(err);
-      res.send({ error: "error retrieving prompts" });
+      res.json({ error: "error retrieving prompts" });
+    });
+});
+
+// @route   GET api/prompts/random
+// @desc    GET a random Prompt
+// @access  Public
+router.get("/random", (req, res) => {
+  Prompt.aggregate([
+    // req.query.category_id ? { $match: { category: ObjectId(req.query.category_id) } } : null,
+    { $match: req.query.category_id ? { category: ObjectId(req.query.category_id) } : {} },
+    { $sample: { size: 1 } },
+    {
+      $lookup: { from: "categories", localField: "category", foreignField: "_id", as: "category" }
+    },
+    { $unwind: "$category" },
+    {
+      $project: {
+        "category._id": 0,
+        "category.reported": 0,
+        "category.__v": 0,
+        "category.prompts": 0 // should be able to get rid of this one
+      }
+    }
+  ])
+    .then(results => res.send(results))
+    .catch(err => {
+      console.log(err);
+      res.json({ error: "error retrieving a random prompt" });
     });
 });
 
