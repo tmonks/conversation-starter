06423a95bd8ff612544d64e3bba84ebdbf2a53e5
Tom Monks <monksy@gmail.com>
2020-03-12 14:57:12 -0400

    New background & improved responsiveness of prompts

--- client/src/components/PromptDisplay.js
+++ client/src/components/PromptDisplay.js
@@ -1,22 +1,23 @@
 import React from "react";
 import "./PromptDisplay.scss";
+import "./ContentContainer.scss";
 import { Jumbotron, Button, Spinner } from "reactstrap";
 
 function PromptDisplay(props) {
   return (
-    <Jumbotron className={props.isLoading ? "loading" : ""}>
+    <div className={"prompt-container " + (props.isLoading ? "loading" : "")}>
       {props.isLoading ? (
         <Spinner />
       ) : (
         <div>
           <p className="category-title">{props.prompt ? props.prompt.category.title : ""}</p>
           <h3 className="prompt-text">{props.prompt ? props.prompt.text : "Loading..."}</h3>
-          <Button color="dark" size="lg" onClick={props.clickHandler}>
+          <Button color="dark" size="lg" className="next-button" onClick={props.clickHandler}>
             Next
           </Button>
         </div>
       )}
-    </Jumbotron>
+    </div>
   );
 }
 
