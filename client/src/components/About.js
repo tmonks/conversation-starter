0045a33ce0821bea1a764f7ac0a4ce78a899a620
Tom Monks <monksy@gmail.com>
2020-03-18 13:43:56 -0400

    Updated About copy and favicon

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,9 +5,19 @@ const About = () => {
   return (
     <div className="content-container">
       <h4>About</h4>
-      <p> Have you ever noticed how hard it is to get your kids to tell you about their day? </p>
       <p>
-        by <a href="http://tommonks.net">Tom Monks</a>
+        Conversation starters are a great way to get your kids talking, to add some variety to your
+        conversations, and to help your family connect through more meaningful conversations.
+      </p>
+      <p>
+        If you have any favorite conversation starters you'd like to see added, please submit them
+        using the Add link above. All submissions will be reviewed before being added to the app.
+      </p>
+      <p>Developed by Tom Monks. Please contact me with any feedback or suggestions.&nbsp;</p>
+      <p>
+        <a href="http://tommonks.net/#contactSection" target="_blank">
+          http://tommonks.net
+        </a>
       </p>
     </div>
   );
